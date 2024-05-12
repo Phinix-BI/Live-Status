872ab0c10cf49b18f6904c0bb1109c71075e69b1
Biswajit Dey <biswajitdey.one@gmail.com>
2024-05-12 09:10:55 +0530

    (Production) :  try to fix a mongodb timeout problem for production

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 const MONGO_DB_USER = process.env.MONGO_DB_USER || '';
 const NODE_ENV = process.env.NODE_ENV || '';
 const MONGO_DB_PASSWORD = process.env.MONGO_DB_PASSWORD || '';
-const MONGO_URL = `mongodb+srv://${MONGO_DB_USER}:${MONGO_DB_PASSWORD}@cluster0.bzs9o0m.mongodb.net/Status-test`;
+const MONGO_URL = `mongodb+srv://${MONGO_DB_USER}:${MONGO_DB_PASSWORD}@cluster0.bzs9o0m.mongodb.net/Status-test?directConnection=true`;
 const SERVER_PORT = process.env.PORT ? Number(process.env.PORT) : 5000;
 const MONGO_URL_LOCAL = `mongodb+srv://${MONGO_DB_USER}:${MONGO_DB_PASSWORD}@cluster0.bzs9o0m.mongodb.net/Status-test`;
 
